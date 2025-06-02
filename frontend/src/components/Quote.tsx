8f016375c02d6836daffc885acb8578c881af4dc
akghosh111 <ghoshkiran2017@gmail.com>
2025-06-02 14:53:55 +0530

    added components, desgined signup quote

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,27 @@
+
+export const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+        <div className="flex justify-center">
+
+            <div className="max-w-lg">
+                <div className="text-2xl font-bold">
+
+                “There is nothing to writing. All you do is sit down at a typewriter and bleed.”
+                </div>
+                <div className="max-w-lg text-sm font-semibold text-left mt-4">
+
+                    ― Ernest Hemingway
+                </div>
+                <div className="max-w-lg text-sm font-light text-slate-500">
+
+                    American Novelist
+                </div>
+
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,8 +1,18 @@
+import { Quote } from "../components/Quote"
 
 export const Signup = () => {
   return (
     <div>
-      Signup
+        <div className="grid grid-cols-2">
+
+            <div>
+
+            </div>
+            <div className="invisible lg:visible">
+              <Quote/>
+            </div>
+            
+        </div>
     </div>
   )
 }
