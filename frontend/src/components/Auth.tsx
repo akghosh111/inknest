08d2599da1fed9010bb2a5b69a948a10f545c713
akghosh111 <ghoshkiran2017@gmail.com>
2025-06-02 16:20:46 +0530

    auth page

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -2,7 +2,7 @@ import type { ChangeEvent } from "react";
 import  { useState} from "react";
 import { Link } from "react-router-dom"
 import type { SingupInput } from "@anukiranghosh/inknest-common";
-export const Auth = ({type}: {type: "signup" | "signin"}) => {
+export const Auth = ({ type }: { type: "signup" | "signin" }) => {
     const [postInputs, setPostInputs] = useState<SingupInput>({
         name: "",
         email: "",
@@ -12,36 +12,44 @@ export const Auth = ({type}: {type: "signup" | "signin"}) => {
     <div className="h-screen flex justify-center flex-col">
         <div className="flex justify-center">
             <div>
+                <div className="px-10">
+                    <div className="text-3xl font-extrabold">
+                        Create an account
+                    </div>
+                    <div className="text-slate-500">
+                        Already have an account? 
+                        <Link className="pl-2 underline" to={"/signin"}>Login</Link>
+                        
+                    </div>
 
-                <div className="text-3xl font-extrabold">
-                    Create an account
                 </div>
-            <div className="text-slate-500">
-                Already have an account? 
-                <Link className="pl-2 underline" to={"/signin"}>Login</Link>
-                
-            </div>
-            <LabelledInput label="Name" placeholder="Tirtha Sarkar" onChange={(e) => {
-                setPostInputs({
-                    ...postInputs,
-                    name: e.target.value
-                })
 
-            }}/>
-            <LabelledInput label="Email" placeholder="tirtha@gmail.com" onChange={(e) => {
-                setPostInputs({
-                    ...postInputs,
-                    email: e.target.value
-                })
+                    <div className="pt-8">
+                        <LabelledInput label="Name" placeholder="Tirtha Sarkar" onChange={(e) => {
+                            setPostInputs({
+                                ...postInputs,
+                                name: e.target.value
+                            })
 
-            }}/>
-            <LabelledInput label="Password" type= {"password"} placeholder="******" onChange={(e) => {
-                setPostInputs({
-                    ...postInputs,
-                    password: e.target.value
-                })
+                        }}/>
+                        <LabelledInput label="Email" placeholder="tirtha@gmail.com" onChange={(e) => {
+                            setPostInputs({
+                                ...postInputs,
+                                email: e.target.value
+                            })
 
-            }}/>
+                        }}/>
+                        <LabelledInput label="Password" type= {"password"} placeholder="******" onChange={(e) => {
+                            setPostInputs({
+                                ...postInputs,
+                                password: e.target.value
+                            })
+
+                        }}/>
+                        <button type="button" className="mt-6 w-full text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">{ type === "signup" ? "Sign up" : "Sign in"}</button>
+                        
+                    </div>
+                    
             </div>
         </div>
       
@@ -61,7 +69,7 @@ function LabelledInput ({ label, placeholder, onChange,type }: LabelledInputType
     return (
 
     <div>
-            <label className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
+            <label className="block mb-2 text-sm  text-gray-900 font-semibold pt-4">{label}</label>
             <input onChange={onChange} type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder={placeholder} required />
     </div>
     )
