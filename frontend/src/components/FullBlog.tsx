d71258686702f95d58175182b481dec0950da503
akghosh111 <ghoshkiran2017@gmail.com>
2025-06-04 16:02:36 +0530

    added about the author card

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -1,5 +1,6 @@
 import type { Blog } from "../hooks"
 import { Appbar } from "./Appbar"
+import { Avatar } from "./BlogCard"
 
 export const FullBlog = ({ blog }: {blog: Blog}) => {
   return (
@@ -7,7 +8,7 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
         <Appbar/>
         <div className="flex justify-center">
 
-            <div className="grid grid-cols-12 px-10 w-full pt-20 max-w-screen-2xl">
+            <div className="grid grid-cols-12 px-10 w-full pt-20 max-w-screen-xl">
             
                 <div className="col-span-8">
                     <div className="text-5xl font-extrabold">
@@ -21,7 +22,25 @@ export const FullBlog = ({ blog }: {blog: Blog}) => {
                     </div>
                 </div>
                 <div className="col-span-4">
-                    hello
+                    <div className="text-slate-600 text-lg">
+
+                        Author
+                    </div>
+                    <div className="flex w-full">
+                        <div className="pr-4 flex flex-col justify-center">
+                            <Avatar size="big" name={blog.author.name || "Anonymous"}/>
+                        </div>
+                    <div>
+
+                        <div className="text-xl font-bold">
+
+                        {blog.author.name || "Anonymous"}
+                        </div>
+                        <div className="pt-2 text-slate-500">
+                            Random catchphrase about the author's ability to grab user's attention
+                        </div>
+                        </div>
+                    </div>
                 </div>
             
             </div>
